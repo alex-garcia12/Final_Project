a26d38a10085affa2fba5b6d094a3db9ed4a279f
Alex Garcia <alex.garcia@csu.fullerton.edu>
2020-05-12 14:41:37 -0700

    app now asks for permissions, added commented out viewmodel components, collaborator names, and commented out directory components

--- app/src/main/java/com/example/final_project/MainActivity.kt
+++ app/src/main/java/com/example/final_project/MainActivity.kt
@@ -1,3 +1,7 @@
+//Moustapha Said 888907524
+//Alex Garcia 802297556
+//Ruiwen Zhang 887649846
+
 package com.example.final_project
 
 import android.Manifest
@@ -6,29 +10,38 @@ import android.annotation.TargetApi
 import android.content.pm.PackageManager
 import android.media.MediaRecorder
 import android.os.Build
-//import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
-//import android.support.v4.app.ActivityCompat
-//import android.support.v4.content.ContextCompat
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import com.example.final_project.R
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
 import java.io.IOException
+import androidx.lifecycle.ViewModelProviders
+
 
 class MainActivity : AppCompatActivity() {
+
+    //private var viewModel: RecorderViewModel? = null
 
     private var output: String? = null
     private var mediaRecorder: MediaRecorder? = null
     private var state: Boolean = false
     private var recordingStopped: Boolean = false
+    //private val dir: File = File(Environment.getExternalStorageDirectory().absolutePath + "/soundrecorder/")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        checkPermissions()
+
+//        if(dir.exists()){
+//            val count = dir.listFiles().size
+//            output = Environment.getExternalStorageDirectory().absolutePath + "/soundrecorder/recording"+count+".mp3"
+//        }
 
         mediaRecorder = MediaRecorder()
         output = Environment.getExternalStorageDirectory().absolutePath + "/recording.mp3"
@@ -58,8 +71,19 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun checkPermissions() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+            != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
+            != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.RECORD_AUDIO), 0)
+        }
+    }
+
     private fun startRecording() {
         try {
+            //viewModel?.startRecording()
             mediaRecorder?.prepare()
             mediaRecorder?.start()
             state = true
@@ -75,8 +99,9 @@ class MainActivity : AppCompatActivity() {
     @TargetApi(Build.VERSION_CODES.N)
     private fun pauseRecording() {
         if(state) {
+            //viewModel?.pauseRecording()
             if(!recordingStopped){
-                Toast.makeText(this,"Stopped!", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this,"Recording Paused!", Toast.LENGTH_SHORT).show()
                 mediaRecorder?.pause()
                 recordingStopped = true
                 button_pause_recording.text = "Resume"
@@ -89,7 +114,7 @@ class MainActivity : AppCompatActivity() {
     @SuppressLint("RestrictedApi", "SetTextI18n")
     @TargetApi(Build.VERSION_CODES.N)
     private fun resumeRecording() {
-        Toast.makeText(this,"Resume!", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this,"Recording Resumed", Toast.LENGTH_SHORT).show()
         mediaRecorder?.resume()
         button_pause_recording.text = "Pause"
         recordingStopped = false
@@ -97,9 +122,11 @@ class MainActivity : AppCompatActivity() {
 
     private fun stopRecording(){
         if(state){
+            //viewModel?.stopRecording()
             mediaRecorder?.stop()
             mediaRecorder?.release()
             state = false
+            Toast.makeText(this, "Recording Stopped", Toast.LENGTH_SHORT).show()
         }else{
             Toast.makeText(this, "You are not recording right now!", Toast.LENGTH_SHORT).show()
         }
